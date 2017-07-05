654c00b132d2ce17c206a74aaa5c5cdce831419c
Rincorpes <rincorpes@gmail.com>
2017-07-05 13:57:36 -0400

    Some functions change

--- src/adsense.php
+++ src/adsense.php
@@ -6,14 +6,6 @@
  */
 if ( !function_exists('gb_adsense') ) : 
 	function gb_adsense() {
-?>
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<script>
-	  (adsbygoogle = window.adsbygoogle || []).push({
-	    google_ad_client: "ca-pub-3376736209557061",
-	    enable_page_level_ads: true
-	  });
-	</script>
-<?php
+		include 'scripts/adsense.php';
 	}
 endif;
